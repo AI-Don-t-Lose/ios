9c841329f1618efab27ce4b62e19b69ae256bc13
z3rosmith <52317025+z3rosmith@users.noreply.github.com>
2025-09-12 22:29:49 +0900

    init project

---
+++ AIDL/AIDLApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIDLApp.swift
+//  AIDL
+//
+//  Created by Jinyoung Kim on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AIDLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
